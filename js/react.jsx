145f0e21e38de4d7abb6d580ea033ca621afae00
Kalior <kalioragus@gmail.com>
2017-02-28 22:23:45 +0100

    Draw color clocks using css instead of canvas

--- js/react.jsx
+++ js/react.jsx
@@ -44,7 +44,7 @@ class MainView extends React.Component {
       <div className="main-view row" id="content">
         <div className="top-clock row">
           <div className="large-offset-1 large-10 columns">
-            <RandomClock currentHour={this.state.currentHour} currentMinute={this.state.currentMinute} currentSecond={this.state.currentSecond}/>
+            <HSLClock currentHour={this.state.currentHour} currentMinute={this.state.currentMinute} currentSecond={this.state.currentSecond}/>
           </div>
         </div>
         <div className="all-clocks row">
