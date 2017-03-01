367ec24e22f4c9560aa7fb1bb5dc6accf1e1a7a3
Kalior <kalioragus@gmail.com>
2017-03-01 21:48:07 +0100

    Count time in milliseconds as well

--- js/react.jsx
+++ js/react.jsx
@@ -13,39 +13,45 @@ class MainView extends React.Component {
     var currentHour = parseInt(moment().format('HH'));
     var currentMinute = parseInt(moment().format('mm'));
     var second = parseInt(moment().format('ss'));
+    var millisecond = parseInt(moment().format('SSS'));
 
     var clockTypes = [
       RandomClock, ColorClock, WrongOrderClock, HSLClock, BarClock
     ];
 
-    this.state = {currentHour: currentHour, currentMinute: currentMinute, currentSecond: second, timer: null,
-      topClockIndex: 0, clockTypes: clockTypes};
+    this.state = {currentHour: currentHour, currentMinute: currentMinute, currentSecond: second, currentMillisecond: millisecond,
+      timer: null, topClockIndex: 0, clockTypes: clockTypes};
 
     this.countTime = this.countTime.bind(this);
     this.updateTopClockIndex = this.updateTopClockIndex.bind(this);
   }
   countTime() {
-    var nextSeconds = this.state.currentSecond + 1;
+    var nextMillisecond = this.state.currentMillisecond + 10;
+    var nextSeconds = this.state.currentSecond;
     var nextMinute = this.state.currentMinute;
     var nextHour = this.state.currentHour;
-    if (nextSeconds >= 60) {
-      nextSeconds = 0;
-      nextMinute++;
-      if (nextMinute >= 60) {
-        nextMinute = 0;
-        nextHour++;
-        if (nextHour >= 24) {
-          nextHour = 0;
+    if (nextMillisecond >= 1000) {
+      nextMillisecond = nextMillisecond % 1000;
+      nextSeconds++;
+      if (nextSeconds >= 60) {
+        nextSeconds = 0;
+        nextMinute++;
+        if (nextMinute >= 60) {
+          nextMinute = 0;
+          nextHour++;
+          if (nextHour >= 24) {
+            nextHour = 0;
+          }
         }
       }
     }
-    this.setState({currentSecond: nextSeconds, currentMinute: nextMinute, currentHour: nextHour});
+    this.setState({currentSecond: nextSeconds, currentMinute: nextMinute, currentHour: nextHour, currentMillisecond: nextMillisecond});
   }
   componentDidMount() {
     var seed = this.state.currentMinute;
     var rand = require('random-seed').create(seed);
     var index = rand.intBetween(0, this.state.clockTypes.length-1);
-    this.setState({timer: setInterval(this.countTime, 100), topClockIndex: index});
+    this.setState({timer: setInterval(this.countTime, 10), topClockIndex: index});
   }
   componentWillUnmount() {
     clearInterval(this.state.timer);
@@ -55,26 +61,32 @@ class MainView extends React.Component {
   }
   render() {
     var clocksWithRow = [];
-    for (var i = 0; i < this.state.clockTypes.length; i++) {
+    var numberOfClocks = this.state.clockTypes.length - 1;
+    for (var i = 0; i < numberOfClocks; i++) {
       clocksWithRow.push(
         <div className="large-3 columns click-clock" key={i} onClick={this.updateTopClockIndex.bind(this, i)}>
           {React.createElement(this.state.clockTypes[i], {currentHour: this.state.currentHour, currentMinute: this.state.currentMinute,
-            currentSecond: this.state.currentSecond})}
+            currentSecond: this.state.currentSecond, currentMillisecond: this.state.currentMillisecond})}
         </div>
       );
     }
+    // Extra for the last item as we want an added class on that one.
+    clocksWithRow.push(
+        <div className="large-3 columns end click-clock" key={numberOfClocks} onClick={this.updateTopClockIndex.bind(this, numberOfClocks)}>
+          {React.createElement(this.state.clockTypes[i], {currentHour: this.state.currentHour, currentMinute: this.state.currentMinute,
+            currentSecond: this.state.currentSecond, currentMillisecond: this.state.currentMillisecond})}
+        </div>
+      );
     return (
       <div className="main-view row" id="content">
         <div className="top-clock row">
           <div className="large-offset-1 large-10 columns">
             {React.createElement(this.state.clockTypes[this.state.topClockIndex], {currentHour: this.state.currentHour, currentMinute: this.state.currentMinute,
-              currentSecond: this.state.currentSecond})}
+              currentSecond: this.state.currentSecond, currentMillisecond: this.state.currentMillisecond})}
           </div>
         </div>
         <div className="all-clocks row">
           {clocksWithRow}
-          <div className="large-3 columns">
-          </div>
         </div>
       </div>
     );
