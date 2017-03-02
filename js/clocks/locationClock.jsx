6848befa5faabff05023142571d66afb81363d35
Kalior <kalioragus@gmail.com>
2017-03-02 15:23:44 +0100

    Correct the description for location clock

--- js/clocks/locationClock.jsx
+++ js/clocks/locationClock.jsx
@@ -36,7 +36,7 @@ export default class LocationClock extends React.Component {
   }
   render() {
     var description = "Since a lot of the clocks here actually display time, even if so in a confounding way, this one does not. " +
-      "Instead, it uses geolocation to find your location and then displays your latitude, longitude, and altitude rounded down " +
+      "Instead, it uses geolocation to find your location and then displays your latitude, longitude, and altitude rounded " +
       "to the closest integer as time. Some tips to reading this one: if the first number is large that probably means that your " +
       "angular distance to the equator also is large. Furthermore, if the second number is large that most likely indicates that " +
       "your east-west distance to Greenwich is great. I don't think the last number requires any further explanation.";
