2610cfa04eff94602ac0abadd32b60c26c0d4e3f
Kalior <kalioragus@gmail.com>
2018-04-19 23:32:58 +0200

    Add desktop notification to code/food clock

--- js/clocks/codeFoodClock.jsx
+++ js/clocks/codeFoodClock.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import Notification from "react-web-notification";
 import DigitalClock from "../templates/digitalClock.jsx";
 import _ from "lodash";
 
 export default class CodeFoodClock extends React.Component {
-  withinRange = (currentHour, currentMinute, targetHour) => {
-    return (
-      (currentHour === targetHour && currentMinute <= 15) ||
-      (currentHour === targetHour - 1 && currentMinute >= 45)
-    );
-  };
+  constructor(props) {
+    super(props);
 
-  render() {
-    const { currentMinute, currentHour } = this.props;
+    const { currentHour, currentMinute, currentSecond } = props;
+    const options = {
+      tag: currentHour + ":" + currentSecond + ":" + currentSecond,
+      lang: 'en'
+    }
+
+    const time = this.checkTime(props);
+    this.state = {
+      time: time,
+      ignore: true,
+      options: options
+    }
+  }
+
+  componentWillReceiveProps (newProps) {
+    if (newProps.currentMinute != this.props.currentMinute) {
+      const { oldTime } = this.state;
+      let { ignore, options } = this.state;
+      const { currentHour, currentMinute, currentSecond } = newProps;
+
+      const newTime = this.checkTime(newProps);
+
+      if (oldTime !== newTime) {
+        ignore = false;
+        options = {
+          tag: currentHour + ":" + currentSecond + ":" + currentSecond,
+          lang: 'en'
+        }
+      } else {
+        ignore = true;
+      }
+      this.setState({time: newTime, ignore: ignore, options: options})
+    }
+  }
+
+  checkTime = (props) => {
+    const { currentSecond, currentMinute, currentHour } = props;
+
     let time = "Code";
     if (
       _.some([8, 12, 16, 20], t =>
@@ -20,13 +53,33 @@ export default class CodeFoodClock extends React.Component {
     ) {
       time = "Food";
     }
+    return time
+  }
 
+  withinRange = (currentHour, currentMinute, targetHour) => {
+    return (
+      (currentHour === targetHour && currentMinute <= 15) ||
+      (currentHour === targetHour - 1 && currentMinute >= 45)
+    );
+  };
+
+  render() {
+    const { isTop } = this.props;
+    const { time, ignore, options } = this.state;
     let description = `Inspired by Skalman's clock (from Bamse, a Swedish cartoon),
       tells him when to eat and when to sleep.  This clock works similarly, but
       instead of being targeted towards a tortoise capable of inventing almost
       anything, it equally capable programmers.  Basically, as a programmer,
       you only need to know two when to code and when to eat, and this clock
       tells you exactly that.`;
+
+    let notification = <Notification
+        ignore={ignore && !isTop}
+        timeout={5000}
+        title={time}
+        askAgain={false}
+        options={options}
+      />
 
     return (
       <div className="clock">
@@ -35,6 +88,7 @@ export default class CodeFoodClock extends React.Component {
           name={"CodeFood Clock"}
           description={description}
         />
+        {notification}
       </div>
     );
   }
