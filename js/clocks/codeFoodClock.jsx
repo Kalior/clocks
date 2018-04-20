0825216cfead4e3083ecf3911e7ecfc6455a4d03
Kalior <kalioragus@gmail.com>
2018-04-20 19:24:31 +0200

    Remove web-notifcation dependency

--- js/clocks/codeFoodClock.jsx
+++ js/clocks/codeFoodClock.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Notification from "react-web-notification";
 import DigitalClock from "../templates/digitalClock.jsx";
 import _ from "lodash";
 
@@ -8,38 +7,58 @@ export default class CodeFoodClock extends React.Component {
     super(props);
 
     const { currentHour, currentMinute, currentSecond } = props;
-    const options = {
-      tag: currentHour + ":" + currentSecond + ":" + currentSecond,
-      lang: 'en'
-    }
 
     const time = this.checkTime(props);
     this.state = {
-      time: time,
-      ignore: true,
-      options: options
+      time: time
     }
+  }
+
+  componentDidMount() {
+    Notification.requestPermission();
   }
 
   componentWillReceiveProps (newProps) {
     if (newProps.currentMinute != this.props.currentMinute) {
       const { time } = this.state;
-      let { ignore, options } = this.state;
       const { currentHour, currentMinute, currentSecond } = newProps;
 
       const newTime = this.checkTime(newProps);
 
       if (time !== newTime) {
-        ignore = false;
-        options = {
-          tag: currentHour + ":" + currentSecond + ":" + currentSecond,
-          lang: 'en'
-        }
-      } else {
-        ignore = true;
+        this.sendNotification(newTime);
       }
-      this.setState({time: newTime, ignore: ignore, options: options})
+
+      this.setState({time: newTime})
     }
+  }
+
+  sendNotification = (time) => {
+    // From https://developer.mozilla.org/en-US/docs/Web/API/Notifications_API/Using_the_Notifications_API
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      return
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(time);
+      setTimeout(notification.close.bind(notification), 5000);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification(time);
+          setTimeout(notification.close.bind(notification), 5000);
+
+        }
+      });
+    }
+    // Finally, if the user has denied notifications and you
+    // want to be respectful there is no need to bother them any more.
   }
 
   checkTime = (props) => {
@@ -65,7 +84,7 @@ export default class CodeFoodClock extends React.Component {
 
   render() {
     const { isTop } = this.props;
-    const { time, ignore, options } = this.state;
+    const { time } = this.state;
     let description = `Inspired by Skalman's clock (from Bamse, a Swedish cartoon),
       which tells him when to eat and when to sleep.  This clock works similarly, but
       instead of being targeted towards a tortoise capable of inventing almost
@@ -73,15 +92,6 @@ export default class CodeFoodClock extends React.Component {
       you only need to know two things: when to code and when to eat.  This clock
       tells you exactly that. Also gives a friendly notification, so you don't miss
       either event.`;
-
-    // let notification = <Notification
-    //     ignore={ignore && isTop}
-    //     timeout={5000}
-    //     title={time + " time!"}
-    //     askAgain={false}
-    //     options={options}
-    //   />
-    // {notification}
 
     return (
       <div className="clock">
